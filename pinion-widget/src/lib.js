085dc6e06de7de5f377d944a32a18a4fe7b6e1aa
Jan Mrázek <email@honzamrazek.cz>
2021-06-02 23:39:49 +0200

    Remove extra logging

--- pinion-widget/src/lib.js
+++ pinion-widget/src/lib.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import {PinionWidget} from './pinion-widget'
 
 export const setup = (element, config) => {
-    console.log("Setting up pinion!");
-    console.log(PinionWidget);
     ReactDOM.render(<PinionWidget {...config} />, element);
 }
 
