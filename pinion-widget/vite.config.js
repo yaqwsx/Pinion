91549f828a9390ca18bd1cf250810a318745f6ec
Jan Mrázek <email@honzamrazek.cz>
2026-06-08 16:08:02 +0200

    Fix browser widget bundle build

--- pinion-widget/vite.config.js
+++ pinion-widget/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
+  esbuild: {
+    charset: 'ascii',
+  },
   plugins: [
     react(),
     tailwindcss(),
